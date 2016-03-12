e4de7bf14bd336a7e87b4b4f2660646dfe72fc8b
huangyedi2012 <huangyedi2012@gmail.com>
2016-03-12 11:37:00 +0800

    exercise greedy: modify dijkstra to meet the requirement of directed graphy

--- greedy/dijkstra/dijkstra.py
+++ greedy/dijkstra/dijkstra.py
@@ -22,12 +22,12 @@ def dijkstra(vex: list, edge: dict, ov,cost:dict,path:dict):
         if not nv:
             break;
         av.append(nv)  # add next vex to added list
-        rv.remove(nv)
+        rv.remove(nv)  # remove added vex from rest list
         # update cost dict
         for v in rv:
-            if nv in edge[v]:
-                if not cost[v] or (cost[v] and cost[nv] + edge[v][nv] < cost[v]):  # if there have no path from ov to v
-                    cost[v] = cost[nv] + edge[v][nv]
+            if v in edge[nv]:
+                if not cost[v] or (cost[v] and cost[nv] + edge[nv][v] < cost[v]):  # if there have no path from ov to v
+                    cost[v] = cost[nv] + edge[nv][v]
                     path[v] = nv
     return path
 
@@ -43,25 +43,34 @@ def chooseshortest(cost: dict, av: list, ov):  # return shortest vex
 
 
 if __name__ == '__main__':
-    vex = ["1", "2", "3", "4", "5", "6"]
+    vex = ["v1", "v2", "v3", "v4", "v5", "v6","v7"]
     MAX = 100
     edge = {
-        vex[0]: {vex[1]: 10, vex[3]: 30, vex[4]: 45},
-        vex[1]: {vex[0]: 10, vex[2]: 50, vex[4]: 40, vex[5]: 25},
-        vex[2]: {vex[1]: 50, vex[4]: 35, vex[5]: 15},
-        vex[3]: {vex[0]: 30, vex[5]: 20},
-        vex[4]: {vex[0]: 45, vex[1]: 40, vex[2]: 35, vex[5]: 55},
-        vex[5]: {vex[1]: 25, vex[2]: 15, vex[3]: 20, vex[4]: 55}
+        vex[0]: {vex[1]: 20, vex[2]: 50, vex[3]: 30},
+        vex[1]: {vex[2]: 25, vex[5]: 70},
+        vex[2]: {vex[3]: 40, vex[4]: 25, vex[5]: 50},
+        vex[3]: {vex[5]: 55},
+        vex[4]: {vex[5]: 10, vex[6]: 70},
+        vex[5]: {vex[6]: 50}
     }
     ov = vex[0]  # origin vex
     cost={}
     path = {}
     dijkstra(vex, edge, ov,cost,path)
     for v in cost:
-        print("cost is:%d   %s"%(cost[v],v),end="")
+        pathstack = []
         pv = path[v]
         while pv != ov:
-            print("--->%s"%(pv),end="")
+            pathstack.append(pv)
             pv = path[pv]
-        print("--->%s"%(pv))
+        pathstack.append(pv)
+        print("%s--->%s cost is:%d   "%(ov,v,cost[v]),end="")
+        psv = pathstack.pop()
+        while psv:
+            print("%s-->"%(psv),end="")
+            if len(pathstack):
+                psv = pathstack.pop()
+            else:
+                psv = None
+        print(v)
 
